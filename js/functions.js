424818890d361d0aaede13a0c44b948b8ad853dc
unknown <romina-velez@hotmail.com>
2023-02-28 22:57:22 -0300

    proyecto frontend

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+
+document.getElementById('btn_color_verde').addEventListener('click', function() {
+    console.log("Cambio de color verde");
+    document.body.style.backgroundColor = '#7ECC49';
+});
+
+document.getElementById('btn_color_rojo').addEventListener('click', function() {
+    console.log("Cambio de color rojo");
+    document.body.style.backgroundColor = '#FF8D85';
+});
+
+document.getElementById('btn_color_blue').addEventListener('click', function() {
+    console.log("Cambio de color azul claro");
+    document.body.style.backgroundColor = '#96C3EB';
+});
+
+document.getElementById('btn_color_grey').addEventListener('click', function() {
+    console.log("Cambio de color gris");
+    document.body.style.backgroundColor = '#B8B8B8';
+});
+
+
+
+
